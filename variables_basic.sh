8282284168065515e31288797c13215145a53e7f
Tomás Proaño <67541868+tomasproanop@users.noreply.github.com>
2023-07-02 15:37:53 +0200

    Add files via upload

---
+++ variables_basic.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#myname="Bicho"
+#myage="6"
+
+#echo "Hi, my name is $myname."
+#echo "I'm $myage years old."
+
+
+#word="great"
+
+#echo "Linux is $word."
+#echo "Cats are $word."
+#echo "Coffee is $word."
+
+#now=$(date)
+
+#echo  "The system time and date is:"
+#echo $now
+
+name="Tomas"
+now=$(date)
+
+echo "Hi $name"
+echo "The system time and date is:"
+echo $now
+echo "Your username is: $USER"
